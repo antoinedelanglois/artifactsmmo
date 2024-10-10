617aa0b8a19fe124c0318bcffe9bdde11c00f37e
Antoine de Langlois <antoine.de.langlois@gmail.com>
2024-10-10 22:44:41 +0200

    refacto(constants)

--- constants.py
+++ constants.py
@@ -1,9 +1,5 @@
 # Server url
 SERVER = "https://api.artifactsmmo.com"
-EQUIPMENTS_SLOTS = ['weapon', 'shield', 'helmet', 'body_armor', 'leg_armor', 'boots', 'ring1', 'ring2',
-                    'amulet', 'artifact1', 'artifact2', 'artifact3']
-EQUIPMENTS_TYPES = ['weapon', 'shield', 'helmet', 'body_armor', 'leg_armor', 'boots', 'ring',
-                    'amulet', 'artifact']
 SLOT_TYPE_MAPPING = {
     'weapon': ['weapon'],
     'shield': ['shield'],
@@ -13,9 +9,10 @@ SLOT_TYPE_MAPPING = {
     'boots': ['boots'],
     'ring': ['ring1', 'ring2'],
     'amulet': ['amulet'],
-    'artifact': ['artifact1', 'artifact2', 'artifact3'],
-    # Add other mappings if necessary
+    'artifact': ['artifact1', 'artifact2', 'artifact3']
 }
+EQUIPMENTS_TYPES = list(SLOT_TYPE_MAPPING.keys())
+EQUIPMENTS_SLOTS = [x for slot in SLOT_TYPE_MAPPING.values() for x in slot]
 EXCLUDED_MONSTERS = ["cultist_acolyte", "cultist_emperor", "lich", "bat"]
 SPAWN_COORDINATES = (0, 0)
 BANK_COORDINATES = (4, 1)
